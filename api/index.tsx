1cc5be34fa606bfb0c587f4a7ca02d237f79b8b0
Ben Bassler <mail@benbaessler.com>
2024-05-27 04:29:23 +0100

    feat: use custom cast hash

--- api/index.tsx
+++ api/index.tsx
@@ -7,6 +7,7 @@ import { handle } from "frog/vercel";
 import { init, fetchQuery } from "@airstack/node";
 import { config } from "dotenv";
 import { getSCVQuery } from "../lib/constants.js";
+import { getCast } from "../lib/neynar.js";
 config();
 
 init(process.env.AIRSTACK_API_KEY!);
@@ -17,6 +18,7 @@ init(process.env.AIRSTACK_API_KEY!);
 // }
 
 type State = {
+	castHash: string | null;
 	txHash: string | null;
 	indexed: boolean;
 };
@@ -30,6 +32,7 @@ export const app = new Frog<State>({
 	assetsPath: "/",
 	basePath: "/api",
 	initialState: {
+		castHash: null,
 		txHash: null,
 		indexed: false,
 	},
@@ -57,9 +60,21 @@ export const app = new Frog<State>({
 	verify: "silent",
 }) as any;
 
+app.frame("/", (c) => {
+	return c.res({
+		image: <></>,
+		intents: [
+			<Button action="/ticket/0x1f87f72d06ba1ae45cc87574d656d0ed918315a8">
+				View Ticket
+			</Button>,
+		],
+	});
+});
+
 // @ts-ignore
-app.frame("/", neynarMiddleware, (c) => {
+app.frame("/ticket/:hash", neynarMiddleware, (c) => {
 	const {
+		req,
 		deriveState,
 		previousState,
 		transactionId,
@@ -67,12 +82,14 @@ app.frame("/", neynarMiddleware, (c) => {
 		frameData,
 	}: any = c;
 
+	const castHash = req.path.split("/")[req.path.split("/").length - 1];
+
 	// Mock data
-	const cast = {
-		author: {
-			username: "benbassler.eth",
-		},
-	};
+	// const cast = {
+	// 	author: {
+	// 		username: "benbassler.eth",
+	// 	},
+	// };
 	const channel = "memes";
 	const tokenPrice = 1450;
 	const tokenSymbol = "DEGEN";
@@ -85,27 +102,23 @@ app.frame("/", neynarMiddleware, (c) => {
 
 	// @ts-ignore
 	const state = deriveState((previousState) => {
+		if (castHash) previousState.castHash = castHash;
 		if (transactionId !== "0x") previousState.txHash = transactionId;
 		if (indexed) previousState.indexed = true;
 	});
 
 	const getImage = async () => {
-		if (!["0x", null].includes(state.txHash)) {
+		if (!["0x", null].includes(previousState.txHash)) {
 			return `${process.env.BASE_URL}/tx-success.png`;
 		}
 
-		let socialCapitalValue = "-";
-		// TODO: fix frameData and remove this
-		const castHash = "0x1f87f72d06ba1ae45cc87574d656d0ed918315a8";
+		const cast = await getCast(castHash);
 
-		// TODO remove !
-		if (!frameData) {
-			const scvQuery = await fetchQuery(getSCVQuery(castHash));
-			socialCapitalValue =
-				scvQuery.data.FarcasterCasts.Cast[0].socialCapitalValue.formattedValue.toFixed(
-					2
-				);
-		}
+		const scvQuery = await fetchQuery(getSCVQuery(castHash));
+		const socialCapitalValue =
+			scvQuery.data.FarcasterCasts.Cast[0].socialCapitalValue.formattedValue.toFixed(
+				2
+			);
 
 		return (
 			<div
@@ -131,7 +144,10 @@ app.frame("/", neynarMiddleware, (c) => {
 					}}
 				>
 					<div style={{ display: "flex", justifyContent: "space-between" }}>
-						<span>Cast by {cast.author.username}</span>
+						<div style={{ display: "flex", alignItems: "center" }}>
+							{/* TODO: add pfp */}
+							<span>Cast by {cast.author.username}</span>
+						</div>
 						<span>/{channel}</span>
 					</div>
 					<div
@@ -191,7 +207,7 @@ app.frame("/", neynarMiddleware, (c) => {
 	const getIntents = () => {
 		return [
 			<Button>Buy Ticket</Button>,
-			<Button.Reset>Refresh</Button.Reset>,
+			<Button.Reset>↻</Button.Reset>,
 			<Button action="/details/memes">Game Details</Button>,
 		];
 	};
@@ -204,13 +220,15 @@ app.frame("/", neynarMiddleware, (c) => {
 
 // @ts-ignore
 app.frame("/details/:channel", (c) => {
-	const { req } = c;
+	const { req, previousState } = c;
 
 	// mock data
 	const prizePool = 90123;
 	const prizePoolUSD = 1234.56;
 	const txCount = 829;
-	const timeUntilTradingHalt = "5 hours"
+	const timeUntilTradingHalt = "5 hours";
+
+	const channel = req.path.split("/")[req.path.split("/").length - 1];
 
 	return c.res({
 		image: (
@@ -237,9 +255,7 @@ app.frame("/details/:channel", (c) => {
 						alignItems: "center",
 					}}
 				>
-					<span>
-						cast.game x {req.path.split("/")[req.path.split("/").length - 1]}
-					</span>
+					<span>cast.game x {channel}</span>
 					<span>Prize Pool:</span>
 					<div
 						style={{
@@ -270,8 +286,11 @@ app.frame("/details/:channel", (c) => {
 			</div>
 		),
 		intents: [
-			<Button action="/">Return</Button>,
-			<Button.Link href="https://cast.game/about">Learn more</Button.Link>,
+			<Button.Link href="https://cast.game/about">cast.game</Button.Link>,
+			<Button.Link href={`https://warpcast.com/~/channel/${channel}`}>
+				/{channel}
+			</Button.Link>,
+			<Button action={`/ticket/${previousState.castHash}`}>↩</Button>,
 		],
 	});
 });
