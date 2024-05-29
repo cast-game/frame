af1336f924f8f2df9d70d5aa13a0a2c4377c5e3d
Ben Bassler <mail@benbaessler.com>
2024-05-29 18:44:33 +0100

    chore: update constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,12 @@
+import { base, baseSepolia } from "viem/chains";
+
+export const useMainnet = process.env.USE_MAINNET === "true";
+export const chain = useMainnet ? base : baseSepolia;
+
+export const ticketsAddress = "0x6e063e33112a0f3a4182af1969259e283dc305b7";
+// TODO: prob move to db
+export const gameAddress = "0xe93426c24999504d3a9ad3dd69cea6fb24faa7b7";
+
 export const getSCVQuery = (castHash: string) => `{
     FarcasterCasts(
       input: {filter: {hash: {_eq: "${castHash}"}}, blockchain: ALL}
