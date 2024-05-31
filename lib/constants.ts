fd8ba1fb5a963ecaf92617cf1be44ef0431c6fb9
Ben Bassler <mail@benbaessler.com>
2024-05-31 18:58:30 +0100

    chore: update contract addresses

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,9 +4,9 @@ export const useMainnet = process.env.USE_MAINNET === "true";
 export const chain = useMainnet ? base : baseSepolia;
 export const chainId = useMainnet ? "eip155:8453" : "eip155:84532";
 
-export const ticketsAddress = "0x89184eb56b6c724b379b02ef8baa7fbdac4d02e3";
+export const ticketsAddress = "0xbf45933b41fa7733a8cb5b94fc4791cd4f1d0967";
 // TODO: prob move to db
-export const gameAddress = "0xd141cbbfe8db842c2d9a8564fb7a3c751d461bbb";
+export const gameAddress = "0x9d18a76c3609479968c43fbebee82ed81f6620d2";
 
 export const getSCVQuery = (castHash: string) => `{
     FarcasterCasts(
