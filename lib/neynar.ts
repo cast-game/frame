dda6ef3cdf24c6bade8fc36d52a79d8bd0b674f4
Ben Baessler <mail@benbaessler.com>
2024-06-03 19:33:33 +0100

    chore: update neynar helpers

--- lib/neynar.ts
+++ lib/neynar.ts
@@ -5,21 +5,24 @@ config();
 export const neynarClient = new NeynarAPIClient(process.env.NEYNAR_API_KEY!);
 
 export const getCast = async (castHash: string) => {
-	const res = await neynarClient.lookUpCastByHashOrWarpcastUrl(
-		castHash,
-		"hash"
-	);
-	return res.cast;
+  const res = await neynarClient.lookUpCastByHashOrWarpcastUrl(
+    castHash,
+    "hash"
+  );
+  return res.cast;
 };
 
 export const getUser = async (fid: number) => {
-	const res = await neynarClient.fetchBulkUsers([fid]);
-	return res.users[0];
-}
+  const res = await neynarClient.fetchBulkUsers([fid]);
+  return res.users[0];
+};
 
 export const getChannel = async (parentUrl: string) => {
-	const res = await neynarClient.lookupChannel(parentUrl, {
-		type: "parent_url",
-	});
-	return res.channel;
+  const res = await neynarClient.lookupChannel(parentUrl, {
+    type: "parent_url",
+  });
+  return res.channel;
 };
+
+export const getUsersFromAddresses = async (addresses: string[]) =>
+  await neynarClient.fetchBulkUsersByEthereumAddress(addresses);
