21b5a2ec38f3a96c5d0cae60ad8529933eb9372c
Ben Baessler <mail@benbaessler.com>
2024-06-08 13:34:38 +0100

    feat: api endpoint

--- lib/constants.ts
+++ lib/constants.ts
@@ -8,6 +8,8 @@ export const ticketsAddress = "0xbf45933b41fa7733a8cb5b94fc4791cd4f1d0967";
 // TODO: prob move to db
 export const gameAddress = "0x9d18a76c3609479968c43fbebee82ed81f6620d2";
 
+export const apiEndpoint = "https://api-production-9d5d.up.railway.app/"
+
 export const getSCVQuery = (castHash: string) => `{
     FarcasterCasts(
       input: {filter: {hash: {_eq: "${castHash}"}}, blockchain: ALL}
