48f428c55c05bb9a1f636e4446d359f9d5319607
Ben Bassler <mail@benbaessler.com>
2024-06-08 17:10:39 +0100

    chore: update contract addresses

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,9 +4,9 @@ export const useMainnet = process.env.USE_MAINNET === "true";
 export const chain = useMainnet ? base : baseSepolia;
 export const chainId = useMainnet ? "eip155:8453" : "eip155:84532";
 
-export const ticketsAddress = "0xbf45933b41fa7733a8cb5b94fc4791cd4f1d0967";
+export const ticketsAddress = "0x85e80330806bd6c9032a2dFA5eb40bAAba030d94";
 // TODO: prob move to db
-export const gameAddress = "0x9d18a76c3609479968c43fbebee82ed81f6620d2";
+export const gameAddress = "0x3DC173846E9aBD600119095046f0feEa21ef58b4";
 
 export const apiEndpoint = "https://api-production-9d5d.up.railway.app/"
 
